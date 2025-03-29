c2bdd4e4f87f343237c3a045b254899d433be4db
josemhc <jose_mig.hernandez@uao.edu.co>
2025-03-29 10:41:38 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-# Deshabilitar systemd-resolved
+echo "Cambiando DNS a 8.8.8.8"
+
+# Deshabilitar systemd-resolved y establecer DNS manualmente
 sudo systemctl stop systemd-resolved
 sudo systemctl disable systemd-resolved
 sudo rm -f /etc/resolv.conf
 echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
 
 # Actualizar paquetes
-sudo apt-get update && sudo apt-get upgrade -y
+echo "Actualizando paquetes..."
+sudo apt update && sudo apt upgrade -y
 
- export DEBIAN_FRONTEND=noninteractive
-  sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+# Agregar la clave GPG y el repositorio de HashiCorp
+echo "Agregando clave GPG de HashiCorp..."
+wget -qO- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
-  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
-  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+echo "Agregando repositorio de HashiCorp..."
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
-  sudo apt-get update
-  sudo apt-get install -y terraform ansible
+# Instalar Terraform
+echo "Instalando Terraform..."
+sudo apt update && sudo apt install terraform -y
 
-# Ejecutar Terraform en la maquina control
-terraform init
-terraform apply -auto-approve
+# Instalar Ansible
+echo "Instalando Ansible..."
+sudo apt install ansible -y
+
+# Verificar instalación
+echo "Verificando instalación de Terraform..."
+terraform -v
